b32b266cc9d21777110c6cb9eb030b70aee7dec7
jiangjf <34030327@qq.com>
2021-05-09 19:34:37 +0800

    'init'

---
+++ src/com/jiangjf/game/StartGame.java
@@ -0,0 +1,10 @@
+package com.jiangjf.game;
+
+import javax.swing.*;
+
+public class StartGame {
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+        jFrame.setTitle("贪吃蛇 by jiangjf");
+    }
+}
