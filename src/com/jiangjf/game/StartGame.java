b6c16bb5525a075515b485694a494234372b47c3
jiangjf <34030327@qq.com>
2021-05-09 20:12:59 +0800

    init

--- src/com/jiangjf/game/StartGame.java
+++ src/com/jiangjf/game/StartGame.java
@@ -5,6 +5,15 @@ import javax.swing.*;
 public class StartGame {
     public static void main(String[] args) {
         JFrame jFrame = new JFrame();
+        // 设置窗体的标题
         jFrame.setTitle("贪吃蛇 by jiangjf");
+        // 设置窗体的位置和大小
+        jFrame.setBounds(10, 20, 800, 800);
+        // 关闭窗口，退出程序
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        // 设置窗体大小不可调
+        jFrame.setResizable(false);
+        // 默认窗体隐藏，需要将窗体显示出来
+        jFrame.setVisible(true);
     }
 }
