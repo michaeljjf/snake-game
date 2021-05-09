1cade8f96528150f0e674833df66c358176e8635
jiangjf <34030327@qq.com>
2021-05-09 20:29:06 +0800

    窗口居中

--- src/com/jiangjf/game/StartGame.java
+++ src/com/jiangjf/game/StartGame.java
@@ -1,6 +1,7 @@
 package com.jiangjf.game;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class StartGame {
     public static void main(String[] args) {
@@ -8,7 +9,14 @@ public class StartGame {
         // 设置窗体的标题
         jFrame.setTitle("贪吃蛇 by jiangjf");
         // 设置窗体的位置和大小
-        jFrame.setBounds(10, 20, 800, 800);
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = dimension.width;
+        int height = dimension.height;
+        int jFrameWidth = 800;
+        int jFrameHeight = 800;
+        int x = (width - jFrameWidth) / 2;
+        int y = (height - jFrameHeight) / 2;
+        jFrame.setBounds(x, y, jFrameWidth, jFrameHeight);
         // 关闭窗口，退出程序
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         // 设置窗体大小不可调
